add7f4f17d60e0cf0b56dd15919e3ab8aa7a61aa
CristianMorales1999 <cm9225064@gmail.com>
2025-03-31 21:10:37 -0500

    Cambio de campos de tabla users y eliminación de tabla user_statuses

--- database/migrations/2024_12_02_000014_create_users_table.php
+++ database/migrations/2024_12_02_000014_create_users_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_status_id')
-                ->constrained('user_statuses')
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
+            $table->boolean('is_active')->default(true);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
